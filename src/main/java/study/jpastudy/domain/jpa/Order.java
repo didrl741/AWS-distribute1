caa7dfc2a8efa9f48ce1f1dec6c800c2223ae42e
Yanggi Song <didrl741@gmail.com>
2022-11-22 13:29:56 +0900

    main application.yml 다시설정

--- src/main/java/study/jpastudy/domain/jpa/Order.java
+++ src/main/java/study/jpastudy/domain/jpa/Order.java
@@ -18,7 +18,9 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
 
     @Embedded
     private Address address;
